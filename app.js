615fd466a899772f3a296183e921d2df303d40d3
Lucien Cartier-Tilet <lucien@phundrak.com>
2022-12-05 11:57:08 +0100

    Nicer interface

--- app.js
+++ app.js
@@ -4,13 +4,24 @@ const app = Vue.createApp({
   data() {
     return {
       items: null,
-    }
+    };
   },
   mounted() {
-    axios.get('https://phundrak.github.io/EXAM_THYP_22-23/omeka.json')
-         .then((response) => this.items = response)
-         .catch((error) => console.log(error));
-  }
+    axios
+      .get('https://phundrak.github.io/EXAM_THYP_22-23/omeka.json')
+      .then((response) => {
+        this.items = response.data.map((i) => {
+          return { ...i, show: true };
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  methods: {
+    toggleItem(item) {
+      console.log('CLICK');
+      item.show = !item.show;
+    },
+  },
 });
 
-app.mount("#app");
+app.mount('#app');
